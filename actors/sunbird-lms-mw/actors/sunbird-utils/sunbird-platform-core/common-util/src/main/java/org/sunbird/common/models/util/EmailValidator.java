37c4ec77d8f521b0da4f3ef6a2e9e60d4a0d40ff
karthik-tarento <karthikeyan.rajendran@tarento.com>
2021-02-18 18:05:37 +0530

    Updated Email Pattern

--- actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/EmailValidator.java
+++ actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/EmailValidator.java
@@ -13,7 +13,7 @@ public class EmailValidator {
 
   private static Pattern pattern;
   private static final String EMAIL_PATTERN =
-      "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+      "^[_A-Za-z0-9-\\+.]+(\\.[_A-Za-z0-9-]+)*@"
           + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
   private EmailValidator() {}
